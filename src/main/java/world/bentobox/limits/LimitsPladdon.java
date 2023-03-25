f0727586a32beca1156181ddc5129c59ae72d49d
tastybento <tastybento@wasteofplastic.com>
2023-03-25 09:54:26 -0700

    Update annotations

--- src/main/java/world/bentobox/limits/LimitsPladdon.java
+++ src/main/java/world/bentobox/limits/LimitsPladdon.java
@@ -1,6 +1,5 @@
 package world.bentobox.limits;
 
-import org.bukkit.plugin.java.annotation.dependency.Dependency;
 import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
@@ -12,9 +11,8 @@ import world.bentobox.bentobox.api.addons.Pladdon;
  * @author YellowZaki
  */
 
-@Plugin(name="Pladdon", version="1.0")
+@Plugin(name="Limits", version="1.0")
 @ApiVersion(ApiVersion.Target.v1_16)
-@Dependency(value = "BentoBox")
 public class LimitsPladdon extends Pladdon {
     
     @Override
